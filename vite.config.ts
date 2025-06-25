7667e208804c8c302da99814a3c2f7a7a6f322e6
zulazri zulkarnain <39299864+zulazriz@users.noreply.github.com>
2025-06-25 17:27:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,11 +22,4 @@ export default defineConfig({
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
-     server: {
-        host: '0.0.0.0',
-        port: 5173, // optional for local dev
-        hmr: {
-            host: 'multibinarezq-production.up.railway.app',
-        }
-    },
 });
